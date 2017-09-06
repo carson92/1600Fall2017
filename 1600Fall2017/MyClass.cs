a1b0baad013a35abb74d71ab03c2590d8959e820
Unknown <carsoncobabe92@gmail.com>
2017-09-06 14:40:44 -0600

    Conventions and Syntax Homework

---
+++ 1600Fall2017/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	string myString = "hello";
+
+	int myInt = 5;
+
+	// Use this for initialization
+	void Start () {
+		//this is a comment
+		print(myString);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
