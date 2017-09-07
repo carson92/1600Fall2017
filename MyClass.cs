dbb6bf900d0d9314476e310b3c4e81b859fa918e
carson92 <31220717+carson92@users.noreply.github.com>
2017-09-06 18:16:10 -0600

    Add files via upload

--- MyClass.cs
+++ MyClass.cs
@@ -11,7 +11,18 @@ public class MyClass : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//this is a comment
+		//
 		print(myString);
+
+		//A coding convention is a set of guidelines for a specific programming language. Also known as syntax, this is a set of rules that you need to follow so you don't have errors and the computer can understand as well as people. 
+		//Exapmle 1. When starting a code, assigning it to a specific program, you need to start with using insert.program
+		//Example 2. When starting a new line, you should type void Start
+		//Example 3. After void Start in a line you should put (parameters)
+		//Example 4. Then {functions}
+		//Example 5. When adding parameters you start with the object, then put my object = "assign to object" For example bottle myBottle = "water"
+		//Rule 1. Always end a line with ;
+		//Rule 2. Always close paranthesis
+		
 	}
 	
 	// Update is called once per frame
