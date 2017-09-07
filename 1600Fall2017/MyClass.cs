bf00be62d2d40a401856ecc5f4dd0d85a767901f
Unknown <carsoncobabe92@gmail.com>
2017-09-06 18:04:48 -0600

    added assignment to MyClass

---
+++ 1600Fall2017/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	string myString = "hello";
+
+	int myInt = 5;
+
+	// Use this for initialization
+	void Start () {
+		//this is a comment
+		//
+		print(myString);
+
+		//A coding convention is a set of guidelines for a specific programming language. Also known as syntax, this is a set of rules that you need to follow so you don't have errors and the computer can understand as well as people. 
+		//Exapmle 1. When starting a code, assigning it to a specific program, you need to start with using insert.program
+		//Example 2. When starting a new line, you should type void Start
+		//Example 3. After void Start in a line you should put (parameters)
+		//Example 4. Then {functions}
+		//Example 5. When adding parameters you start with the object, then put my object = "assign to object" For example bottle myBottle = "water"
+		//Rule 1. Always end a line with ;
+		//Rule 2. Always close paranthesis
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
