def91da948b810cb82f11846049d01f5d22089d0
carson92 <31220717+carson92@users.noreply.github.com>
2017-09-09 19:40:07 -0600

    Add files via upload

---
+++ MATH OPERATORS.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MATHOPERATORS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//A math operator is when you add something into the code and make it so that some math or any variation to the numebrs happen. 
+	int number = 5;
+		number += 10;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int number = 7; 
+		number -= 3;
+	}
+
+	void Update () {
+		int number = 9;
+		number += 6;
+	}
+	void Update () {
+		int number = 14;
+		number *= 2;
+	}
+	void Update () {
+		int number = 6;
+		number /= 3;
+	}
+	void Update () {
+		int number = 7;
+		number += 3;
+	}
+	void Update () {
+		int number = 13;
+		number -= 3;
+	}
+	void Update () {
+		int number = 8; 
+		number -= 4;
+	}
+	void Update () {
+		int number = 5;
+		number *= 7;
+	}
+	void Update () {
+		int number = 7;
+		number /= 3.5;
+	}
+}
