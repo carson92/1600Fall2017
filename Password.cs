da3d1de51f6b124ee52b0121ca33b9bdebb531c4
carson92 <31220717+carson92@users.noreply.github.com>
2017-09-14 19:41:29 -0600

    Add files via upload

---
+++ Password.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Password : MonoBehaviour {
+
+	public Text input;
+	public bool canPlayGame = false;
+	public string password = "G@mepl@y";
+	public string password2 = "carsonscool";
+	
+	void Update () {
+		if(input.text == password) {
+			print("You know the password");
+			canPlayGame = true;
+		} 
+		
+		else {
+			print("Incorrect password");
+		}
+
+		if(canPlayGame) {
+			print("Playing Game");
+		}
+
+		else {
+			print("Cant play yet, enter a correct password.");
+		}
+
+		if(input.text == password2) {
+			print("You got the second password.");
+		}
+
+		else {
+			print("You are obviously not the user. Go away.")
+		}
+
+		}
+}
