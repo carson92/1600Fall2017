ea553241b3c086889a016e9d6f7853a5a6dbd024
Unknown <carsoncobabe92@gmail.com>
2017-09-20 20:21:03 -0600

    While Loop

--- 1600Fall2017/Assets/Whileloop.cs
+++ 1600Fall2017/Assets/Whileloop.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class Whileloop : MonoBehaviour {
 
 	public bool canPlay = true;
+	public bool canJump = true;
+	public bool canRun = true;
+	public bool canFly = true; 
+	public bool canLeap = true;
+	public bool canEat = true;
+	public bool canFart = true;
+	public bool canDie = true;
+	public bool canRoll = true;
+	public bool canLive = true;
 
 	void Start() {
 		StartCoroutine(PlayGame());
+		StartCoroutine(JumpCharacter());
+		StartCoroutine(RunCharacter());
+		StartCoroutine(FlyCharacter());
+		StartCoroutine(LeapCharacter());
+		StartCoroutine(CharacterEating());
+		StartCoroutine(CharacterFarting());
+		StartCoroutine(CharacterDying());
+		StartCoroutine(CharacterRolling());
+		StartCoroutine(CharacterLiving());
+
 	}
 
 	IEnumerator PlayGame () {
@@ -17,5 +36,68 @@ public class Whileloop : MonoBehaviour {
 			yield return new WaitForSeconds(0.1f);
 		}
 		print("End Game");
+	}
+	IEnumerator JumpCharacter () {
+		while (canJump) {
+			print("Jumping");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Not Jumping");
+	}
+	IEnumerator RunCharacter () {
+		while (canRun) {
+			print("Running");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Not Running");
+	}
+	IEnumerator FlyCharacter () {
+		while (canFly) {
+			print("Flying");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Crashing");
+	}
+	IEnumerator LeapCharacter () {
+		while (canLeap) {
+			print("Leaping");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Falling");
+	}
+	IEnumerator CharacterEating () {
+		while (canEat) {
+			print("Eating");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Starving");
+	}
+	IEnumerator CharacterFarting () {
+		while (canFart) {
+			print("Farting");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Not Farting");
+	}
+	IEnumerator CharacterDying () {
+		while (canDie) {
+			print("Dying");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Living");
+	}
+	IEnumerator CharacterRolling () {
+		while (canRoll) {
+			print("Rolling");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Staying still");
+	}
+	IEnumerator CharacterLiving () {
+		while (canLive) {
+			print("Living");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Dead");
 	}
 }
