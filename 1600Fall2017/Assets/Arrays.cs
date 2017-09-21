a5d68d0ca8cde277a146beacad4bfc890329078d
Unknown <carsoncobabe92@gmail.com>
2017-09-20 21:08:37 -0600

    Array

---
+++ 1600Fall2017/Assets/Arrays.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+	public int[] {87 89 74 32};
+	public int[] {50 52 54};
+	public int[] {89 64 22};
+	public int[] {27 38 99};
+	public int[] {89 340 34};
+	public int[] {32 20 88};
+	public int[] {689 4940 4332};
+	public int[] {9392 23 3232};
+	public int[] {1234 568 0987};
+	public int[] {3939 203049 2023}; 
+
+	void Start () {
+		
+		
+	}
+	
+	void Update () {
+		
+	}
+}
