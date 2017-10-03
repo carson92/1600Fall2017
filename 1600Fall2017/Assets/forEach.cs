aac5316882d5627653cb01b185475670005c89d9
Unknown <carsoncobabe92@gmail.com>
2017-10-02 21:26:23 -0600

    For Each Script

---
+++ 1600Fall2017/Assets/forEach.cs
@@ -0,0 +1,18 @@
+﻿//A foreach loop is a loop that can be carried out multiple times. The foreach loop is the easiest to make it so that it wont error, but it is also limited in some ways. 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forEach : MonoBehaviour {
+
+	
+	void Update () {
+		 string[] pets = { "dog", "cat", "bird" };
+
+        foreach (string value in pets)
+        {
+            print("value");
+		
+	}
+}
+}
