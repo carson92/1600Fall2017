d24ce83bc552fbf82cd946eb7b9edd73aa9fc428
Unknown <carsoncobabe92@gmail.com>
2017-10-02 21:44:58 -0600

    Lists

---
+++ 1600Fall2017/Assets/Lists1.cs
@@ -0,0 +1,20 @@
+﻿//Lists give objects the ability to get different things under their title. For example If you were to get guns, you could make a list to get the names of the guns and limit how many you can hold. 
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Lists1 : MonoBehaviour {
+	
+	
+		public List<GameObject> myWeapons;
+		public List<GameObject> cars;
+		public List<GameObject> games;
+		public List<GameObject> songs;
+		public List<GameObject> buildings;
+		public List<GameObject> cities;
+		public List<GameObject> countries;
+		public List<GameObject> worlds;
+		public List<GameObject> multiverses;
+		public List<GameObject> yeah;
+
+		
+	}
