c1eada06bf7ad6711dc95b1857cbfbcee82634d9
Unknown <carsoncobabe92@gmail.com>
2017-10-06 00:11:52 -0600

    Scope and Access

---
+++ 1600Fall2017/Assets/ScopeAndAccess.cs
@@ -0,0 +1,32 @@
+﻿//Scope and access modifiers just classify what is in the class, so if it will be public and visible to everything or if it will just be visible to the code. So in this particular code, the ones will be visible, while the twos will just be scoped in the class so visible just to the code. 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccess : MonoBehaviour {
+	string thing1 = "Thing1";
+	string car1 = "car1";
+	string house1 = "house1";
+	string building1 = "building1";
+	string city1 = "city1";
+	string state1 = "state1";
+	string country1 = "country1";
+	string continent1 = "continent1";
+	string world1 = "world1";
+	string universe1 = "universe1";
+	void Box() {
+		int newScore = 10; 
+		print(thing1);
+		string thing2 = "Thing2";
+		string car2 = "car2";
+		string house2 = "house2";
+		string building2 = "building2";
+		string city2 = "city2";
+		string state2 = "state2";
+		string country2 = "country2";
+		string continent2 = "continent2";
+		string world2 = "world2";
+		string universe2 = "universe2";
+
+}
+}
