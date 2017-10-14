5931e8b148a010dd00e981817ef8f815c58cc2a5
Unknown <carsoncobabe92@gmail.com>
2017-10-13 21:08:08 -0600

    Return

---
+++ 1600Fall2017/Assets/returns.cs
@@ -0,0 +1,18 @@
+﻿//A returen will return the informstion to its caller. So it is referrencing an integer or some sort of information to a different place in the script. 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class returns : MonoBehaviour {
+	void Main(string[]) {
+		Console.WriteLine(returnString());
+	}
+
+	string returnString(int HotorNot)  {
+		if (HotorNot < 6)
+			return "You are not hot!";
+		if (HotorNot >= 6)
+			return "You are hot enough.";
+
+	}
+}
---
+++ 1600Fall2017/Assets/saveCheckpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class saveCheckpoint : MonoBehaviour {
+
+	public Transform player;
+	Vector3 checkPoint;
+	string[] playerPrefsTitles = {"CheckpointX", "CheckpointY", "CheckpointZ"};
+	 
+	void OnTriggerEnter()
+	{
+		checkPoint = transform.position;
+			for (int i = 0; i < playerPrefsTitles.Length; i++)
+			{
+				PlayerPrefs.SetFloat(playerPrefsTitles[i], checkPoint[i]);
+			}}
+	void start()	{
+		for (int i = 0; i < playerPrefsTitles.Length; i++)
+			{
+					checkPoint[i] = PlayerPrefs.GetFloat(playerPrefsTitles[i]);
+			}
+		player.position = checkPoint;
+	}	
+			
+			
+			
+}
