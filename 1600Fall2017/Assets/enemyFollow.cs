75a6c191c55921701c526b53cf50aa855c3f2ec4
Unknown <carsoncobabe92@gmail.com>
2017-10-18 13:02:43 -0600

    Enemy follow

---
+++ 1600Fall2017/Assets/enemyFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyFollow : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public Transform player;
+	void Update () {
+		agent.destination = player.position;
+	}
+	public void Stun (float _speed) {
+		agent.speed = _speed;
+		
+	}
+}
