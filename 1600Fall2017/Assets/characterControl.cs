3a5a8648075740aa3f095290342565d2d24b9abe
Unknown <carsoncobabe92@gmail.com>
2017-10-17 22:49:18 -0600

    green character move

---
+++ 1600Fall2017/Assets/characterControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterControl : MonoBehaviour {
+	public CharacterController characterController;
+	public float gravity = 3f; 
+	public Vector3 moveVector3;
+	public float speed = 10;
+	public float jumpForce = 50;
+	
+	void FixedUpdate () {
+		moveVector3.y -= gravity * Time.deltaTime;
+		if(characterController.isGrounded){
+			if(Input.GetKey(KeyCode.Space)){
+			moveVector3.y += jumpForce * Time.deltaTime;
+			}
+			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		}
+		
+		characterController.Move(moveVector3);
+		
+	}
+}
