4e00c89fc8cff387a644b2d662b964b0e0205eca
Unknown <carsoncobabe92@gmail.com>
2017-10-18 12:59:45 -0600

    Trigger spawn script

---
+++ 1600Fall2017/Assets/spawnlevel1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnlevel1 : MonoBehaviour {
+ 
+     public Transform Destination;      
+     public string TagList = "|Player|"; 
+ 
+     public void OnTriggerEnter(Collider other)
+     {
+         if (TagList.Contains(string.Format("|{0}|",other.tag))) {
+             other.transform.position = Destination.transform.position;
+             other.transform.rotation = Destination.transform.rotation;
+         }
+     }
+ }
