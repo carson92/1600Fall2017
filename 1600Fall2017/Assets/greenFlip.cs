bed842aefb4d4c6c706a647a567dea4836f9adfe
Unknown <carsoncobabe92@gmail.com>
2017-10-30 11:02:01 -0600

    flip when walk

---
+++ 1600Fall2017/Assets/greenFlip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class greenFlip : MonoBehaviour {
+
+	Quaternion myRotate;
+	Vector3 rotValue;
+	// Use this for initialization
+
+	void Start () {
+		MoveInput.MoveAction += Flip;
+	}
+
+    private void Flip(float obj)
+    {
+		if(obj > 0 )
+			rotValue.y = 330;
+
+		if(obj < 0)
+			rotValue.y = 30;
+
+		myRotate.eulerAngles = rotValue;
+        transform.rotation = myRotate; 
+    }
+}
