5f272af13857d0ef3bde9c45f5249a84feec5677
Unknown <carsoncobabe92@gmail.com>
2017-10-30 11:01:29 -0600

    camera control

---
+++ 1600Fall2017/Assets/cameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+	public Transform attachObject;
+	public GameObject assetArt;
+	public GameObject moreStuff;
+	void OnTriggerEnter()
+	{
+		transform.parent = attachObject;
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = Quaternion.identity;
+		GetComponent<BoxCollider>().enabled = false;
+	}
+
+}
