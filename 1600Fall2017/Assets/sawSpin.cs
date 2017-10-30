0d35347ab2aab6381b0e1130e4874c01d172aa43
Unknown <carsoncobabe92@gmail.com>
2017-10-30 11:00:34 -0600

    saw spin

---
+++ 1600Fall2017/Assets/sawSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sawSpin : MonoBehaviour {
+	public int rotSpeed;
+	
+
+ void Update()
+ {
+     transform.Rotate(Vector3.one * rotSpeed * Time.deltaTime);
+ }
+}
