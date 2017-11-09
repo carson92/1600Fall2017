09d09a4892b30268e12cf4220d4690db9166c3b0
Unknown <carsoncobabe92@gmail.com>
2017-11-08 20:22:21 -0700

    Better death

--- 1600Fall2017/Assets/DeathScript.cs
+++ 1600Fall2017/Assets/DeathScript.cs
@@ -7,15 +7,28 @@ public class DeathScript : MonoBehaviour {
 
 	public float currHealth = 100;
 
+    public float maxCoins = 1000;
+
+    public float currCoins = 0;
+
 	public float healthBarLength;
+    public float coinCollectBar;
+    
 
     public GameObject gameOverUI;
 
 
  void Start () {
      healthBarLength = Screen.width / 2;
+     coinCollectBar = Screen.width / 2;
+
  }
- void Update () {
+
+    void OnGUI(){
+     GUI.Box(new Rect(10, 10, healthBarLength, 20), currHealth + "/" + maxHealth); 
+     GUI.Box(new Rect(10, 40, coinCollectBar, 20), currCoins + "/" + maxCoins);   
+ }
+  void Update () {
      AddjustCurrentHealth(0);
      Death ();
      if (GameObject.FindWithTag ("Player") == null)
@@ -23,12 +36,7 @@ public class DeathScript : MonoBehaviour {
          Application.LoadLevel(Application.loadedLevel);
      }
  }
-    void OnGUI(){
-     GUI.Box(new Rect(10, 10, healthBarLength, 20), currHealth + "/" + maxHealth);    
- }
- 
- 
- public void AddjustCurrentHealth(int adj) {
+  public void AddjustCurrentHealth(int adj) {
              currHealth += adj;    
      
              if (currHealth < 0)
@@ -37,11 +45,22 @@ public class DeathScript : MonoBehaviour {
              if (currHealth > maxHealth)
                      currHealth = maxHealth;
      
-             if (maxHealth < 1)
-                     maxHealth = 1;
-     
              healthBarLength = (Screen.width / 2) * (currHealth / (float)maxHealth);
-     }
+ }
+ 
+
+public void AddjustCurrentCoin(int adj){
+    currCoins += adj;
+        if(currCoins < 0)
+            currCoins = 0;
+        if (currCoins > 0)
+            currCoins = maxCoins;
+        if (maxCoins < 1)
+            maxCoins = 1;
+        coinCollectBar = (Screen.width / 2) * (currHealth /(float)maxCoins);
+}
+
+     
  public void Death() {
      if (currHealth == 0) {
          Destroy(gameObject);
@@ -65,9 +84,14 @@ public class DeathScript : MonoBehaviour {
 			if(currHealth > 100) {
 			currHealth = 100; 
             
-            if(currHealth == 0){
+            if(currHealth == 0){ 
                 gameOverUI.SetActive(true);
+            }
+            if(other.CompareTag("coin")){
+                currCoins += 100;
             }
 		}		
     }
 }
+
+        		
