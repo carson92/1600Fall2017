1bc0bb655831fe2eefcb2f9871e932313122f999
Unknown <carsoncobabe92@gmail.com>
2017-11-13 13:18:06 -0700

    speed move

---
+++ 1600Fall2017/Assets/speedMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedMove : MonoBehaviour {
+public float speed;
+	void Update () {
+		if(gameObject == true)
+transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+	}
+}
