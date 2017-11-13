b269ff7225dfd5756b8f295e906a39b6e0ba80cc
Unknown <carsoncobabe92@gmail.com>
2017-11-13 12:51:07 -0700

    mario like block

---
+++ 1600Fall2017/Assets/block.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour {
+
+	public GameObject speed;
+
+	void Start()
+	{
+		speed.SetActive(false);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		speed.SetActive(true);
+	}
+}
