c4497105a42fb432b9ad0739c360814bcb645c67
Unknown <carsoncobabe92@gmail.com>
2017-11-17 16:56:00 -0700

    hillary sound

---
+++ 1600Fall2017/Assets/hillarySound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hillarySound : MonoBehaviour {
+
+	public AudioSource soundSource;
+	public AudioClip no;
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("banana")){
+			soundSource.PlayOneShot(no);
+		}
+	}
+}
