9a83d7b9d52b4d4ec645431300d774a526ae5ff9
Unknown <carsoncobabe92@gmail.com>
2017-11-17 16:56:15 -0700

    money sound

---
+++ 1600Fall2017/Assets/moneySound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moneySound : MonoBehaviour {
+
+	public AudioSource soundSource;
+	public AudioClip imReallyRich;
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Health")){
+			soundSource.PlayOneShot(imReallyRich);
+		}
+	}}
