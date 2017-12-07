c60eb879338c0573bb34510d1007a956fac63e2d
Unknown <carsoncobabe92@gmail.com>
2017-12-07 00:07:49 -0700

    For loop

---
+++ 1600Fall2017/Assets/formaybe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class formaybe : MonoBehaviour {
+	void Start(){
+	 int[] arr = { 0, 1, 2, 3, 4 };
+        List<int> list = new List<int>();
+
+        for (int x = 5; x < 10; x++)
+        {
+            list.Add(x);
+        }
+
+	}}
+	
