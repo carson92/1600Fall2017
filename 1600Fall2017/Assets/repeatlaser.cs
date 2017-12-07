e2759a3cd39b45dac95e2e8366ac285366a5115c
Unknown <carsoncobabe92@gmail.com>
2017-12-07 00:10:29 -0700

    Repeat Laser

---
+++ 1600Fall2017/Assets/repeatlaser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repeatlaser : MonoBehaviour {
+	public GameObject laser;
+	public float repeatTime = 1f;
+	public GameObject spawnPoint;
+	void Start(){
+		InvokeRepeating("RepeatLaser", repeatTime, repeatTime);
+	}
+	void RepeatLaser(){
+		GameObject.Instantiate(laser);
+		laser.transform.position = spawnPoint.transform.position;
+			}
+}
