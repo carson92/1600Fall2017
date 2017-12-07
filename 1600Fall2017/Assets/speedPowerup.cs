1f08a31d6c80875999c4208b128c220b14f0d1d8
Unknown <carsoncobabe92@gmail.com>
2017-12-07 00:08:07 -0700

    Speed Powerup

---
+++ 1600Fall2017/Assets/speedPowerup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedPowerup : MonoBehaviour {
+public float speed;
+private int time;
+public float seconds;
+private bool isCounting;
+public AudioSource SoundSource;
+public AudioClip speedSound;
+	void Update() {
+		if(isCounting){
+			seconds -= Time.deltaTime;
+		}
+		if(seconds <= 0){
+			isCounting = false;
+			time = time - 1;
+			speed = speed - 5;
+			seconds = seconds + 10;
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("speed")){
+			other.gameObject.SetActive(false);
+			time = time + 1;
+			speed = speed + 5;
+			isCounting = true;
+		}
+	}
+}
