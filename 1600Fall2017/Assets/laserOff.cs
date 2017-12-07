ecc68784ab65fcfe121baa364df4b96565829f5f
Unknown <carsoncobabe92@gmail.com>
2017-12-07 00:08:47 -0700

    Kill laser

---
+++ 1600Fall2017/Assets/laserOff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserOff : MonoBehaviour {
+	public GameObject laser;
+
+	void OnbTriggerEnter (Collider other) {
+		if(other.CompareTag ("laseroff")){
+			laser.SetActive(false);
+		}
+
+	}
+}
